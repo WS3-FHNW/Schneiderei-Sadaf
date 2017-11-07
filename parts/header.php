56c8daec363912e579f9e2f3b72fc59eb6a3d1b6
valendinosaurus <amdt542@gmail.com>
2017-11-07 15:23:53 +0100

    test vale

--- parts/header.php
+++ parts/header.php
@@ -2,5 +2,7 @@
     <div class="container">
         <img src="gx/sadaf1.png" alt="Schneiderei Sadaf Logo">
         <h1>Express Schneiderei Sadaf</h1>
+		
+		<!-- test vale -->
     </div>
 </header>
