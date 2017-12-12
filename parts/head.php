fb5ba637a3be3171184a16f7f9b0d11d20b2028e
Hussein Farzi <hussein.farzi@students.fhnw.ch>
2017-12-12 17:04:15 +0100

    Included the Bootstrap, iQuery and Font-Awesome in head file.

--- parts/head.php
+++ parts/head.php
@@ -1,6 +1,10 @@
 <head>
     <meta http-equiv="content-type" content="text/html" charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <script src="jquery-3.2.1.min.js"></script>
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
     <meta name="google-site-verification" content="cjYCfxNRGvjbq0gaU9dePWAVbu-Kbdmak_55hHyr4UE">
     <meta name="description" content="Express Schneiderei Sadaf in Basel: Wir ändern und fertigen nach Ihren Wünschen Kürzungen und Verlängerungen von Stoffen, Wohntextilien und Kleidung, Kunststopfen, Reparaturarbeiten, Leder- und Pelzverarbeitung, Anpassungen aller Art, Umgestaltung von Textilien jeglicher Art, Falknerstrasse 13, 4001 Basel">
     <meta name="keywords" content="Schneiderei,Änderungen,Kürzungen,Verlängerungen,Sadaf,Stoffen,Wohntextilien,Kleidung,Kunststopfen,Leder,Pelzverarbeitung,Anpassungen,Textilien,www.sadaf.ch,Hussein,Abassi,Basel,Pfluggässlein,13,4001,Basel">
