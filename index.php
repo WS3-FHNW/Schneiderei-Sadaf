dbe08c4fb854f4c8c000ad4177911b17b5eecc33
Hussein Farzi <hussein.farzi@students.fhnw.ch>
2017-12-20 01:08:42 +0100

    Change the colors in css file.

--- index.php
+++ index.php
@@ -6,17 +6,19 @@ include('parts/head.php');
 ?>
 
 <body>
-    <div class="container">
-        <div class="wrapper">
-            <div class="mover">
-                
-                <div class="side-image">
-                    
-                </div>
+    <?php
+	    include('parts/header.php');
+	?>
 
-                <div class="content">
-                    <h1>Die beste Qualität</h1>
-                    <h2>Wir ändern und fertigen nach Ihren Wünschen!<h2>
+    <div class="wrapper">
+        <div class="mover">
+            <div class="side-image">
+
+            </div>
+
+            <div class="content">
+                <h1>Die beste Qualität</h1>
+                <h2>Wir ändern und fertigen nach Ihren Wünschen!<h2>
                     <p>
                         Schön, dass Sie unsere Homepage besuchen.
                         Wir möchten Ihr Partner in allen Fragen der
@@ -25,85 +27,22 @@ include('parts/head.php');
                         mit Ihrer Kleidung Ihre Einstellung zum Leben
                         auszudrücken.
                     </p>
-                </div>
+            </div>
 
-                <div class="container">
+            <div class="container">
                     <h3>Öffnungzeiten</h3>
                     <p>
                         Mo.:&emsp;&nbsp;14:00 - 18:30<br>
                         Di.-Fr.:&nbsp;09:00 - 18:30<br>
                         Sa.:&emsp;&nbsp;&nbsp;09:00 - 17:00<br>
                     </p>
-                </div>
-                <div class="menu"></div>
-                <!-- include('parts/menu.php'); -->
             </div>
-        </div>
-	</div>
-	
+            
+            <div class="menu">
 
-<!--
-<div class="menubutton">
-    <a href="#" title="Menu"> Open </a>
-    <a href="#" title="Menu"> Close </a>
-    <nav>
-        <ul>
-            <li><a href="#"> Home </a></li>
-            <li><a href="#"> Über uns </a></li>
-            <li><a href="#"> Dienstleistungen </a></li>
-            <li><a href="#"> Kontakt </a></li>
-        </ul>
-    </nav>
-    <div role="main" id="content">
-        <div class="container">
-            <div class="prtcl-row">
-                <div class="col">
-                    <h1>Unser Motto</h1>
-                    <p>Schneiderei nach Mass</p>
-                </div>
             </div>
-        </div>
-        <section id="iconbox">
-            <div class="container center">
-                <div class="prtcl-row grow">
-                    <div class="col">
-                        <a href="#">
-                            <h3>Home</h3>
-                            <p><i class="fa fa-home-o fa-5x">ICON</i></p>
-                        </a>
-                    </div>
-                    <div class="col">
-                        <a herf="#">
-                            <h3>Über uns</h3>
-                            <i class="fa fa-book-o fa-5x">ICON</i>
-                        </a>
-                    </div>
-                    <div>
-                        <a href="#">
-                            <h3>Dienstleistungen</h3>
-                            <i class="fa fa-handshake-o fa-5x">ICON</i>
-                        </a>
-                    </div>
-                    <div>
-                        <a href="#">
-                            <h3>Kontakt</h3>
-                            <i class="fa fa-envelope-open-o fa-5x">ICON</i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <div class="container white">
-            <div class="prtcl-row">
-                <div class="col">
-                    <p>Made with <i class="fa fa-bug-o fa-5x">ICON</i><a herf="#"> Hussein Farzi</a></p>
-                </div>
-            </div>
+            <!-- include('parts/menu.php'); -->
         </div>
     </div>
-</div>
--->
-
 </body>
-
 </html>
