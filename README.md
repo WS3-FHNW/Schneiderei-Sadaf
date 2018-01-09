13b55f311b4e17686ab1d1cc857147375f666f31
valendinosaurus <amdt542@gmail.com>
2018-01-09 16:14:05 +0100

    cleaned branch

--- README.md
+++
@@ -1,1 +0,0 @@
-# Schneiderei-Sadaf
--- parts/header.php
+++
@@ -1,9 +0,0 @@
- <header>
-    <div class="container">
-    </div>
-
-    <div id="menu-btn">
-    	<i class="fa fa-bars" aria-hidden="true"></i>
-    </div>
-    
-</header>
